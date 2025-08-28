6fa2d1202d00e51c96221afef3621fbbcea296bf
shojib116 <Md. Nazmul Hasan Shajib>
2025-08-28 17:57:20 +0600

    initial nextjs project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+	variable: "--font-inter",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: 'Md. Nazmul Hasan Shajib - Fullstack Developer',
+	description: 'Servant of ALLAH with a vision for making an impact. Fullstack Developer specializing in Go, Node.js, React, and Next.js.',
+	keywords: ['fullstack developer', 'Go', 'Node.js', 'React', 'Next.js', 'competitive programming'],
+	authors: [{ name: 'Md. Nazmul Hasan Shajib' }],
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" >
+			<body
+				className={`${inter.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
