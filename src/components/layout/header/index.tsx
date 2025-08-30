0371c42d767c7324e2963fc464ee3596b9934f98
shojib116 <Md. Nazmul Hasan Shajib>
2025-08-30 15:19:56 +0600

    added logo icon and changed header for mobile navigation

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -3,17 +3,22 @@
 import { useEffect, useState } from "react";
 import { motion } from "motion/react";
 import { ThemeToggle } from "./theme-toggle";
+import LogoIcon from "@/components/icons/logo";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
+import { Menu, X } from "lucide-react";
+import MaxWidthWrapper from "../max-width-wrapper";
 
 const navItems = [
   { href: "#home", label: "Home" },
+  { href: "#projects", label: "Projects" },
   { href: "#about", label: "About" },
   { href: "#skills", label: "Skills" },
-  { href: "#projects", label: "Projects" },
   { href: "#experience", label: "Experience" },
   { href: "#contact", label: "Contact" },
 ];
 
 export default function Navigation() {
+  const [open, setOpen] = useState(false)
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -34,39 +39,66 @@ export default function Navigation() {
       <motion.nav
         initial={{ y: -100 }}
         animate={{ y: 0 }}
-        className={`fixed top-0 w-full z-50 transition-all duration-300 ${
-          isScrolled
-            ? "bg-background/80 backdrop-blur-lg border-b border-border/50"
-            : "bg-transparent"
-        }`}
+        className={`fixed top-0 w-full z-50 transition-all duration-300 ${isScrolled
+          ? "bg-background/80 backdrop-blur-lg border-b border-border/50"
+          : "bg-transparent"
+          }`}
       >
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center py-4">
+        <MaxWidthWrapper>
+          <div className="grid grid-cols-2 lg:grid-cols-[9.375rem_1fr_9.375rem] items-center py-4">
             <motion.div
               whileHover={{ scale: 1.05 }}
-              className="text-xl font-bold text-foreground cursor-pointer"
+              className="text-xl text-left font-bold text-foreground cursor-pointer flex gap-2"
               onClick={() => scrollToSection("#home")}
             >
-              Shajib.dev
+              <LogoIcon className="w-8 h-8 fill-inherit" />
+              <span className="hidden lg:block">Shajib.dev</span>
             </motion.div>
 
-            <div className="hidden md:flex items-center space-x-8">
-              {navItems.map((item) => (
-                <motion.button
-                  key={item.href}
-                  onClick={() => scrollToSection(item.href)}
-                  className="text-muted-foreground hover:text-foreground font-bold transition-colors"
-                  whileTap={{ scale: 0.95 }}
-                >
-                  {item.label}
-                </motion.button>
-              ))}
+            <div className="hidden lg:flex items-center mx-auto space-x-8">
+              <NavItems scrollToSection={scrollToSection} />
             </div>
 
-            <ThemeToggle />
+            <div>
+              <ThemeToggle className="hidden lg:block float-right" />
+              <div className="lg:hidden float-right">
+                <Popover open={open} onOpenChange={setOpen}>
+                  <motion.div
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    <PopoverTrigger>
+                      <motion.div
+                        animate={{ rotate: open ? 90 : 0 }}>
+                        {open ? <X className="h-6 w-6 text-foreground" /> : <Menu className="h-6 w-6 text-foreground" />}
+                      </motion.div>
+                    </PopoverTrigger>
+                  </motion.div>
+                  <PopoverContent className="min-w-30 max-w-60 relative">
+                    <div className="flex flex-col space-y-2 items-start text-left">
+                      <NavItems scrollToSection={scrollToSection} />
+                      <ThemeToggle />
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </div>
+            </div>
           </div>
-        </div>
+        </MaxWidthWrapper>
       </motion.nav>
     </motion.header>
   );
 }
+
+
+function NavItems({ scrollToSection }: { scrollToSection: (s: string) => void }) {
+  return <>{navItems.map((item) => (
+    <motion.button
+      key={item.href}
+      onClick={() => scrollToSection(item.href)}
+      className="text-muted-foreground hover:text-foreground font-bold transition-colors"
+      whileTap={{ scale: 0.95 }}
+    >
+      {item.label}
+    </motion.button>
+  ))}
+  </>
--- src/components/layout/header/theme-toggle.tsx
+++ src/components/layout/header/theme-toggle.tsx
@@ -3,12 +3,16 @@
 import { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "@/lib/contexts/theme-context";
-import { motion } from "motion/react";
+import { type HTMLMotionProps, motion } from "motion/react";
+import { cn } from "@/lib/utils";
 
-export function ThemeToggle() {
-  const { theme, toggleTheme } = useTheme();
+export function ThemeToggle({
+  className,
+  ...props
+}: HTMLMotionProps<"button">) {
+  const { toggleTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-  console.log(theme);
+
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -24,8 +28,12 @@ export function ThemeToggle() {
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
       onClick={toggleTheme}
-      className="relative w-9 h-9 rounded-md border border-input bg-background text-foreground hover:bg-accent hover:text-accent-foreground transition-colors"
+      className={cn(
+        "relative w-9 h-9 rounded-md border border-input bg-background text-foreground hover:bg-accent hover:text-accent-foreground transition-colors",
+        className,
+      )}
       aria-label="Toggle theme"
+      {...props}
     >
       <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
       <Moon className="h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
