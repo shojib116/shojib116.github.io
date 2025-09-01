8cf26376c362f1590520875672a7472acb38ac4e
shojib116 <shojib116@users.noreply.github.com>
2025-09-01 04:29:32 +0000

    deploy: 5f0d3d44d7a81bab1f3a03e2dec5bd10a5a81a11

---
+++ _next/static/chunks/085aa9d5190d62c6.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/98aa4ff57a8c9811.js",
+  "static/chunks/2cf8eda289914e9c.js",
+  "static/chunks/turbopack-a60195f6339bb56a.js"
+])
---
+++ _next/static/chunks/6437001f135aff0b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/19c04f7813400784.js",
+  "static/chunks/2cf8eda289914e9c.js",
+  "static/chunks/turbopack-cf88e3f208d40ecb.js"
+])
